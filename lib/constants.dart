e87f3b2180574b22c66639e48c2d798d378b4410
ritik jain <ritik.200027@gmail.com>
2021-07-15 22:56:31 +0530

    otp page added

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:needs_app/sizeConfig.dart';
 
 const Color kPrimaryColor = Color(0xFFFF7643);
 const Color kPrimaryLightColor = Color(0xFFFFECDF);
@@ -18,3 +19,12 @@ const String kinvalidEmailEroor = 'Email Id is invalid';
 const String kpassNullEroor = 'Please enter your password';
 const String kshortPassEroor = 'password is too short,use min 8 characters';
 const String kmatchPassEroor = 'password doesn\'t match';
+
+final kOTPinputdecoration = InputDecoration(
+    contentPadding: EdgeInsets.symmetric(vertical: getProportionateScreenWidth(15)),
+    enabledBorder: outlineInputBorder(),
+    focusedBorder: outlineInputBorder(),
+    border: outlineInputBorder());
+
+OutlineInputBorder outlineInputBorder() =>
+    OutlineInputBorder(borderRadius: BorderRadius.circular(15), borderSide: BorderSide(color: kTextColor));
